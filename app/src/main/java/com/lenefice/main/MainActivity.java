394841cf1fada7cf62bd63dc500d0378ff75604b
maverickteam2427 <maverickteam2427@rediffmail.com>
2021-07-25 01:42:53 +0530

    Major underhood changes Full code Optimization NOW SUPPORTS ALL BRANDS more efficient fixed display glitch

--- app/src/main/java/com/lenefice/main/MainActivity.java
+++ app/src/main/java/com/lenefice/main/MainActivity.java
@@ -1,9 +1,11 @@
 package com.lenefice.main;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -13,27 +15,83 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button activateButton, button2Min, button5Min,
             button10Min, button30Min, buttonNever, cancelButton;
-
     private TextView textView;
     private CountDownTimer countDownTimer;
-
+    private boolean success, activateStatus, b;
     private int defaultTimeOut;
-    private boolean success, activateStatus;
+
+    private boolean onePlus,asus,vivo,colme,samsung,gock,miui,aosp,others;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
+        toMapComponents();
+        askPermission();
+        justStarted();
+        detectDevice();
 
+        activateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activateDeactivate();
+            }
+        });
+
+        button2Min.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTimerOf(120000);
+            }
+        });
+
+        button5Min.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTimerOf(300000);
+            }
+        });
+
+        button10Min.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTimerOf(600000);
+            }
+        });
+
+        button30Min.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTimerOf(1800000);
+            }
+        });
+
+        buttonNever.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTimerOf(Integer.MAX_VALUE);
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancelTimer();
+            }
+        });
+
+    }
+
+    void toMapComponents() {
+        textView = findViewById(R.id.textView);
         activateButton = findViewById(R.id.activateButton);
         button2Min = findViewById(R.id.button2Min);
         button5Min = findViewById(R.id.button5Min);
@@ -41,133 +99,168 @@ public class MainActivity extends AppCompatActivity {
         button30Min = findViewById(R.id.button30Min);
         buttonNever = findViewById(R.id.buttonNever);
         cancelButton = findViewById(R.id.cancelButton);
+    }
 
-        askPermission();
+    void askPermission() {
+        boolean value;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            value = Settings.System.canWrite(getApplicationContext());
 
-        if(success) {
-            activateButton.setEnabled(true);
+            if (value) {
+                success = true;
+            } else {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+                startActivity(intent);
+            }
+        } else {
+            success = true;
         }
-        else {
+
+        if (success) {
+            activateButton.setEnabled(true);
+        } else {
             activateButton.setEnabled(false);
             Toast.makeText(MainActivity.this, "Please allow permission & launch the app again", Toast.LENGTH_LONG).show();
             finish();
         }
+    }
 
+    void justStarted() {
         button2Min.setEnabled(false);
         button5Min.setEnabled(false);
         button10Min.setEnabled(false);
         button30Min.setEnabled(false);
         buttonNever.setEnabled(false);
         cancelButton.setEnabled(false);
+    }
 
+    void detectDevice() {
+
+        final String ONEPLUSCLOCK = "com.oneplus.deskclock";
+        final String ASUS = "com.asus.deskclock";
+        final String VIVO = "com.android.bbkclock";
+        final String COLME = "com.coloros.alarmclock";
+        final String SAMSUNG = "com.sec.android.app.clockpackage";
+        final String GOCK = "com.google.android.deskclock";
+        final String MIAO = "com.android.deskclock";
+        final String MIUI = "com.miui.gallery";
+
+        Context context = getApplicationContext();
+        PackageManager packageManager = context.getPackageManager();
+
+        if(appInstalledOrNot(context, ONEPLUSCLOCK) && isAppInSystemPartition(packageManager, ONEPLUSCLOCK)) {
+            onePlus=true;
+        }
+        else if(appInstalledOrNot(context, ASUS) && isAppInSystemPartition(packageManager, ASUS)) {
+            asus=true;
+        }
+        else if(appInstalledOrNot(context, VIVO) && isAppInSystemPartition(packageManager, VIVO)) {
+            vivo=true;
+        }
+        else if(appInstalledOrNot(context, COLME) && isAppInSystemPartition(packageManager, COLME)) {
+            colme=true;
+        }
+        else if(appInstalledOrNot(context, SAMSUNG) && isAppInSystemPartition(packageManager, SAMSUNG)) {
+            samsung=true;
+        }
+        else if(appInstalledOrNot(context, GOCK) && isAppInSystemPartition(packageManager, GOCK)) {
+            gock=true;
+        }
+        else if(appInstalledOrNot(context, MIAO) && isAppInSystemPartition(packageManager, MIAO)) {
+            if(appInstalledOrNot(context, MIUI) && isAppInSystemPartition(packageManager, MIUI)) {
+                miui=true;
+            }
+            else {
+                aosp=true;
+            }
+        }
+        else {
+            others=true;
+        }
+    }
+
+    public static boolean appInstalledOrNot(Context context, String uri) {
+        PackageManager pm = context.getPackageManager();
+        List<PackageInfo> packageInfoList = pm.getInstalledPackages(PackageManager.GET_ACTIVITIES);
+        if (packageInfoList != null) {
+            for (PackageInfo packageInfo : packageInfoList) {
+                String packageName = packageInfo.packageName;
+                if (packageName != null && packageName.equals(uri)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean isAppInSystemPartition(PackageManager pm, String packageName) {
+        try {
+            ApplicationInfo ai = pm.getApplicationInfo(packageName, 0);
+            return ((ai.flags & (ApplicationInfo.FLAG_SYSTEM | ApplicationInfo.FLAG_UPDATED_SYSTEM_APP)) != 0);
+        }catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    void getDefaultTimeOut() {
         try {
             defaultTimeOut = Settings.System.getInt(getContentResolver(),
                     Settings.System.SCREEN_OFF_TIMEOUT);
         } catch (Settings.SettingNotFoundException e) {
             e.printStackTrace();
         }
+    }
 
-        activateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(activateStatus) {
-                    activateOnClick();
-                }
-                else {
-                    activateButton.setEnabled(true);
-                    activateButton.setText("Deactivate");
-                    setScreenTimeout(30000);
-
-                    button2Min.setEnabled(true);
-                    button5Min.setEnabled(true);
-                    button10Min.setEnabled(true);
-                    button30Min.setEnabled(true);
-                    buttonNever.setEnabled(true);
-                    cancelButton.setEnabled(false);
-                    textView.setText("Choose Your Profile :-");
-                    activateStatus=true;
-                }
-            }
-        });
-
-        button2Min.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setScreenTimeout(120000);
-                startTimer(120000);
-                if(success) {
-                    presetTimer();
-                }
-                else {
-                    noSuccess();
-                }
-            }
-        });
-
-        button5Min.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setScreenTimeout(300000);
-                startTimer(300000);
-                if(success) {
-                    presetTimer();
-                }
-                else {
-                    noSuccess();
-                }
-            }
-        });
-
-        button10Min.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setScreenTimeout(600000);
-                startTimer(600000);
-                if(success) {
-                    presetTimer();
-                }
-                else {
-                    noSuccess();
-                }
-            }
-        });
-
-        button30Min.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setScreenTimeout(Integer.MAX_VALUE);
-                startTimer(1800000);
-                if(success) {
-                    presetTimer();
-                }
-                else {
-                    noSuccess();
-                }
-            }
-        });
-
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                countDownTimer.cancel();
-                textView.setText("Not Active");
-                resetScreenTimeOut();
-                activateButton.setEnabled(true);
-                activateButton.setText("Activate");
-                button2Min.setEnabled(false);
-                button5Min.setEnabled(false);
-                button10Min.setEnabled(false);
-                button30Min.setEnabled(false);
-                buttonNever.setEnabled(false);
-                cancelButton.setEnabled(false);
-                activateStatus=false;
-            }
-        });
+    void activateDeactivate() {
+        if(activateStatus) {
+            toDeactivate();
+        }
+        else {
+            toActivate();
+        }
     }
 
     private void setScreenTimeout(int milliseconds) {
         Settings.System.putInt(
                 getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT, milliseconds);
+    }
+
+    void showOptionsAsPerDevice() {
+        if(onePlus||colme||vivo) {
+            buttonNever.setEnabled(false);
+        }
+        else if(asus||samsung||others) {
+            buttonNever.setEnabled(false);
+            button30Min.setEnabled(false);
+        }
+        else if(miui) {
+            button30Min.setEnabled(false);
+        }
+    }
+
+    void toDeactivate() {
+        activateButton.setEnabled(true);
+        activateButton.setText("Activate");
+        justStarted();
+        setScreenTimeout(defaultTimeOut);
+        textView.setText("Not Active");
+        activateStatus=false;
+    }
+    void toActivate() {
+        getDefaultTimeOut();
+        activateButton.setEnabled(true);
+        activateButton.setText("Deactivate");
+        setScreenTimeout(30000);
+        button2Min.setEnabled(true);
+        button5Min.setEnabled(true);
+        button10Min.setEnabled(true);
+        button30Min.setEnabled(true);
+        buttonNever.setEnabled(true);
+        cancelButton.setEnabled(false);
+        showOptionsAsPerDevice();
+        textView.setText("Choose Your Profile :-");
+        activateStatus=true;
     }
 
     private void startTimer(int time) {
@@ -190,14 +283,14 @@ public class MainActivity extends AppCompatActivity {
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        activateOnClick();
+                        toDeactivate();
                     }
                 }, 35000);
             }
         }.start();
     }
 
-    void presetTimer() {
+    void asTimerStarted() {
         activateButton.setEnabled(false);
         button2Min.setEnabled(false);
         button5Min.setEnabled(false);
@@ -206,46 +299,21 @@ public class MainActivity extends AppCompatActivity {
         buttonNever.setEnabled(false);
         cancelButton.setEnabled(true);
     }
-    void noSuccess() {
-        button2Min.setEnabled(false);
-        button5Min.setEnabled(false);
-        button10Min.setEnabled(false);
-        button30Min.setEnabled(false);
-        buttonNever.setEnabled(false);
-        cancelButton.setEnabled(false);
-    }
-    void resetScreenTimeOut() {
-        Settings.System.putInt(
-                getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT, defaultTimeOut);
-    }
-    void activateOnClick() {
-        activateButton.setEnabled(true);
-        activateButton.setText("Activate");
-        setScreenTimeout(defaultTimeOut);
-        button2Min.setEnabled(false);
-        button5Min.setEnabled(false);
-        button10Min.setEnabled(false);
-        button30Min.setEnabled(false);
-        buttonNever.setEnabled(false);
-        cancelButton.setEnabled(false);
-        textView.setText("Not Active");
-        activateStatus=false;
-    }
 
-    void askPermission() {
-        boolean value;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-            value = Settings.System.canWrite(getApplicationContext());
-
-            if (value) {
-                success = true;
-            } else {
-                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
-                intent.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
-                startActivity(intent);
-            }
-        } else {
-            success = true;
+    void startTimerOf(int time) {
+        setScreenTimeout(time);
+        startTimer(time);
+        if(success) {
+            asTimerStarted();
         }
+        else {
+            justStarted();
+        }
+    }
+
+    void cancelTimer() {
+        countDownTimer.cancel();
+        justStarted();
+        toDeactivate();
     }
 }
